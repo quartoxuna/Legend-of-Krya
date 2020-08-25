3fca1f6aa4ae7b1e824ad68f7731adb89844e74b
Kai Borowiak <kai.borowiak@secunet.com>
2020-08-25 21:27:23 +0200

    feat: implement SFML example

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	// Create the main window
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Application");
 
-	return 0;
+	// Start the game loop
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// Close window on exit
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		// Clear screen
+		window.clear(sf::Color::Magenta);
+		window.display();
+	}
+
+	return EXIT_SUCCESS;
 }
